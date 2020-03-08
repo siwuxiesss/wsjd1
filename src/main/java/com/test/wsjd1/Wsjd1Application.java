538032c9d416a3d3ef11424dbc10a01b4f9148b5
siwuxiesss <28747467+siwuxiesss@users.noreply.github.com>
2020-03-08 15:06:06 +0800

    Initial commit

---
+++ src/main/java/com/test/wsjd1/Wsjd1Application.java
@@ -0,0 +1,13 @@
+package com.test.wsjd1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Wsjd1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Wsjd1Application.class, args);
+    }
+
+}
