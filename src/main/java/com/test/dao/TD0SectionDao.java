613d051e1c2288263f5e404ddf7eb320d6fe6f28
siwuxiesss <28747467+siwuxiesss@users.noreply.github.com>
2020-03-19 10:11:08 +0800

    添加 @Mapper 注解

--- src/main/java/com/test/dao/TD0SectionDao.java
+++ src/main/java/com/test/dao/TD0SectionDao.java
@@ -1,6 +1,7 @@
 package com.test.dao;
 
 import com.test.entity.TD0Section;
+import org.apache.ibatis.annotations.Mapper;
 
 import java.util.List;
 
@@ -9,6 +10,7 @@ import java.util.List;
  * Date: 2020/3/8
  * Time: 21:15
  */
+@Mapper
 public interface TD0SectionDao {
     /**
      * 查询所有科室信息
