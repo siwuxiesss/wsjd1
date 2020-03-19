d1d8a9dc78aa2e94c893c5ed817705fbf8cfbb52
siwuxiesss <28747467+siwuxiesss@users.noreply.github.com>
2020-03-19 11:29:58 +0800

    添加 @Mapper 注解

--- src/main/java/com/test/Wsjd1Application.java
+++ src/main/java/com/test/Wsjd1Application.java
@@ -1,9 +1,11 @@
-package com.test.wsjd1;
+package com.test;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.test.dao")
 public class Wsjd1Application {
 
     public static void main(String[] args) {
