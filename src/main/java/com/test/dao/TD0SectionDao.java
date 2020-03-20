a3f910c790a283c2f74145732595463b87e8362d
siwuxiesss <28747467+siwuxiesss@users.noreply.github.com>
2020-03-20 09:52:16 +0800

    TD0SectionDao的添加，查找，删除，更新的方法

--- src/main/java/com/test/dao/TD0SectionDao.java
+++ src/main/java/com/test/dao/TD0SectionDao.java
@@ -18,4 +18,36 @@ public interface TD0SectionDao {
      * @return
      */
     List<TD0Section> selectAllSection();
+
+    /**
+     * 插入科室
+     *
+     * @param record
+     * @return
+     */
+    int insert(TD0Section record);
+
+    /**
+     * 通过id查找科室
+     *
+     * @param id
+     * @return
+     */
+    TD0Section selectByPrimaryKey(Integer id);
+
+    /**
+     * 通过id删除科室
+     *
+     * @param id
+     * @return
+     */
+    int deleteByPrimaryKey(Integer id);
+
+    /**
+     * 更新数据
+     *
+     * @param record
+     * @return
+     */
+    int updateByPrimaryKey(TD0Section record);
 }
